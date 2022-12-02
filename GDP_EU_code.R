3b99d47832a77fc2296bfff405bd390221dab39d
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-02 17:37:44 +0100

    adding a path

---
+++ GDP_EU_code.R
@@ -0,0 +1,18 @@
+library(tidymodels)
+library(modeltime)
+library(timetk)   
+library(lubridate)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+library(highcharter)
+library(ggfortify)
+library(coefplot)
+library(boot)
+library(GGally)
+
+
+
+path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(path)
