b2c2dcfd6acf642ee97be7a6b94da0d2d78ef027
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-02 18:40:13 +0100

    quarter as a date

--- GDP_EU_code.R
+++ GDP_EU_code.R
@@ -1,6 +1,7 @@
 library(tidymodels)
 library(modeltime)
 library(timetk)   
+#library(zoo)
 library(lubridate)
 library(dplyr)
 library(tidyverse)
@@ -17,11 +18,20 @@ library(GGally)
 path <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(path)
 
-
 #loading data: GDP
 #source of data: https://ec.europa.eu/eurostat/databrowser/view/namq_10_gdp/default/table?lang=en
 GDP <- read.csv("gdp_quart_eu.csv", stringsAsFactors = F)
 
+#quarter as a date
+GDP$TIME_PERIOD <- as.Date(as.yearqtr(GDP$TIME_PERIOD, format = "%YQ%q"))
+GDP$TIME_PERIOD <- yq(GDP$TIME_PERIOD)                          
+                           
+                           
+
 #filtering data from Poland
 GDP_PL <- GDP[GDP$geo == "PL", ]
 
+
+
+GDP_PL %>%
+  plot_time_series(TIME_PERIOD, OBS_VALUE, .interactive = FALSE)
