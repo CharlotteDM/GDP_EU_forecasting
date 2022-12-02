be59e978b00fcc6dc13d4ce8d962764fa07bab82
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-02 18:24:14 +0100

    filtering data from Poland

--- GDP_EU_code.R
+++ GDP_EU_code.R
@@ -16,3 +16,12 @@ library(GGally)
 
 path <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(path)
+
+
+#loading data: GDP
+#source of data: https://ec.europa.eu/eurostat/databrowser/view/namq_10_gdp/default/table?lang=en
+GDP <- read.csv("gdp_quart_eu.csv", stringsAsFactors = F)
+
+#filtering data from Poland
+GDP_PL <- GDP[GDP$geo == "PL", ]
+
