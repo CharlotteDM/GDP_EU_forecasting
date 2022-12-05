f291d326bf4bf4f30f3683d21cecb5c7f0ea91d6
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-05 17:47:16 +0100

    replacing the data

--- GDP_EU_code.R
+++ GDP_EU_code.R
@@ -31,11 +31,13 @@ GDP_PL <- GDP[GDP$geo == "PL", ]
 #visualization plot time
 GDP_PL %>%
   plot_time_series(TIME_PERIOD, OBS_VALUE, .interactive = T, 
-                   .title = "GDP in Poland", .x_lab = "Date", .y_lab = "current prices (million euro")
+                   .title = "GDP in Poland", .x_lab = "Date", .y_lab = "current prices (million euro)")
 
 #splitting data into train and test sets
 splits <- GDP_PL %>%
-  time_series_split(assess = "3 month", cumulative = TRUE)
+  time_series_split(assess = "3 months", cumulative = TRUE)
+
+?time_series_split
 
 train_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD < "2021-01-01",]
 test_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD > "2021-01-01",]
