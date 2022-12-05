f34ac6262539487f397ab36d15048e42755ff454
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-05 19:42:57 +0100

    autoarima

--- GDP_EU_code.R
+++ GDP_EU_code.R
@@ -23,7 +23,8 @@ setwd(path)
 GDP <- read.csv("gdp_quart_eu.csv", stringsAsFactors = F)
 
 #quarter as a date
-GDP$TIME_PERIOD <- yq(GDP$TIME_PERIOD)                          
+GDP$TIME_PERIOD <- yq(GDP$TIME_PERIOD)   
+
                            
 #filtering data from Poland
 GDP_PL <- GDP[GDP$geo == "PL", ]
@@ -34,17 +35,35 @@ GDP_PL %>%
                    .title = "GDP in Poland", .x_lab = "Date", .y_lab = "current prices (million euro)")
 
 #splitting data into train and test sets
-splits <- GDP_PL %>%
-  time_series_split(assess = "2 year", cumulative = TRUE)
 
-?time_series_split
+#splits <- initial_time_split(GDP_PL, prop = 0.9)
+  
+  
+splits <- GDP_PL %>%time_series_split(assess = "2 years", cumulative = TRUE)
+
 
 #train_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD < "2021-01-01",]
 #test_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD > "2021-01-01",]
 
 #visualization: train & test data
-splits %>%
+vis <- splits %>%
   tk_time_series_cv_plan() %>%
-  plot_time_series_cv_plan(TIME_PERIOD, OBS_VALUE, .interactive = FALSE)
+  plot_time_series_cv_plan(TIME_PERIOD, OBS_VALUE, .interactive = T)
+vis
 
+
+#autoarima
+model_spec <- arima_reg() %>%
+  set_engine("auto_arima")
+# Fit Spec
+model_fit <- model_spec %>%
+  fit(value ~ TIME_PERIOD, data = training(splits))
+model_fit
+
+
+
+mod_fit_arima <- arima_reg() %>%
+  set_engine("auto_arima") %>%
+  fit(log(value) ~ splits$TIME_PERIOD, training(splits))
+?arima_reg
 
