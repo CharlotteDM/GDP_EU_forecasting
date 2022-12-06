32eef61cbb79ace0bc57bee5799680aab9c43367
CharlotteDM <carolinemularczyk@gmail.com>
2022-12-06 14:47:17 +0100

    autoarima (fixed)

--- GDP_EU_code.R
+++ GDP_EU_code.R
@@ -12,6 +12,7 @@ library(ggfortify)
 library(coefplot)
 library(boot)
 library(GGally)
+library(xgboost)
 
 
 
@@ -25,45 +26,34 @@ GDP <- read.csv("gdp_quart_eu.csv", stringsAsFactors = F)
 #quarter as a date
 GDP$TIME_PERIOD <- yq(GDP$TIME_PERIOD)   
 
-                           
 #filtering data from Poland
 GDP_PL <- GDP[GDP$geo == "PL", ]
 
+#choosing only two needed columns
+GDP_PL_new <- GDP_PL %>% select(8,9)
+
 #visualization plot time
-GDP_PL %>%
-  plot_time_series(TIME_PERIOD, OBS_VALUE, .interactive = T, 
-                   .title = "GDP in Poland", .x_lab = "Date", .y_lab = "current prices (million euro)")
+#xyz <- GDP_PL_new %>%
+#  plot_time_series(TIME_PERIOD, OBS_VALUE, .interactive = T, 
+#                   .title = "GDP in Poland", .x_lab = "Date", .y_lab = "current prices (million euro)")
 
 #splitting data into train and test sets
-
-#splits <- initial_time_split(GDP_PL, prop = 0.9)
-  
-  
-splits <- GDP_PL %>%time_series_split(assess = "2 years", cumulative = TRUE)
+splits <- GDP_PL_new %>% time_series_split(assess = "2 years", cumulative = TRUE)
 
 
 #train_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD < "2021-01-01",]
 #test_GDP_PL <- GDP_PL[GDP_PL$TIME_PERIOD > "2021-01-01",]
 
 #visualization: train & test data
-vis <- splits %>%
+splits %>%
   tk_time_series_cv_plan() %>%
-  plot_time_series_cv_plan(TIME_PERIOD, OBS_VALUE, .interactive = T)
+  plot_time_series_cv_plan(TIME_PERIOD, OBS_VALUE, .interactive = TRUE)
+
 vis
 
 
 #autoarima
-model_spec <- arima_reg() %>%
-  set_engine("auto_arima")
-# Fit Spec
-model_fit <- model_spec %>%
-  fit(value ~ TIME_PERIOD, data = training(splits))
-model_fit
-
-
-
 mod_fit_arima <- arima_reg() %>%
   set_engine("auto_arima") %>%
-  fit(log(value) ~ splits$TIME_PERIOD, training(splits))
-?arima_reg
+  fit(OBS_VALUE ~ TIME_PERIOD, training(splits))
 
